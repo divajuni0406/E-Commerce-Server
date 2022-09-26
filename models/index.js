80c139e2bfb2a005d3e93de33a3db55fa514d452
skuukzky14 <idhamdummy1@gmail.com>
2022-09-26 15:47:27 +0700

    feat: test add payment gateway

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const Order = require('./Order')
 const OrderDetail = require('./OrderDetail')
 const Product = require('./Product')
 const Category = require('./Category')
+const PaymentTransaction = require('./PaymentTransaction')
 
 module.exports = {
-    User,Cart,CartDetail,Order,OrderDetail,Product,Category
+    User,Cart,CartDetail,Order,OrderDetail,Product,Category,PaymentTransaction
 }
