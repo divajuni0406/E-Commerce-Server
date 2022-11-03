f24fff4f3cbe90a211150a6560b28d1746284b62
Jothan Zodiathes <jothanzodiathes@gmail.com>
2022-11-03 21:25:29 +0800

    media handling BLOG

--- controller/BlogController.js
+++ controller/BlogController.js
@@ -18,28 +18,48 @@ const getAllArticle = async (req, res) => {
 }
 
 
-const createArticle = async (req, res) => {
+const createArticle = (req, res) => {
     upload(req, res, async (err) => {
         if (err) return res.status(500).json({ message: "Image must one" })
+        try {
+            if (!req.file) {    
+                const imageLink = req.body.imageLink
+                const data = req.body
+                const dataArticle = {
+                    title: data.title,
+                    content: data.content,
+                    category: data.category,
+                    tag: data.tag,
+                    image: imageLink
+                }
+                const createData = await Blog.create(dataArticle)
+                res.status(200).json({
+                    message: "succesfully create data",
+                    data: dataArticle
+                })
+            } else {
+                const image = req.file
+                const imgBlog = `${path}/${image.filename}`
+                const data = req.body
+                const dataArticle = {
+                    title: data.title,
+                    content: data.content,
+                    category: data.category,
+                    tag: data.tag,
+                    image: imgBlog
+                }
+                const createData = await Blog.create(dataArticle)
+                res.status(200).json({
+                    message: "succesfully create data",
+                    data: dataArticle
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            res.send({message:error})
+        }
     })
-        
-    const data = req.body
-    const dataArticle = {
-        title: data.title,
-        content: data.content,
-        category: data.category,
-        tag: data.tag,
-        image: data.image
-    }
-    try {
-        const createData = await Blog.create(dataArticle)
-        res.status(200).json({
-            message: "succesfully create data"
-        })
-    } catch (error) {
-        console.log(error);
-        res.send({message:error})
-    }
+    
 }
 
 const getArticleById = async (req, res) => {
@@ -58,17 +78,46 @@ const getArticleById = async (req, res) => {
 
 const editArticleById = async (req, res) => {
     const id = req.params.id
-    const data = req.body
-    try {
-        const editData = await Blog.updateOne({ _id: id }, {$set:data})
-        res.status(200).json({
-            message: "successfully edit data",
-            result: editData
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({message: "failed to edit data"})
-    }
+    upload(req, res, async (err) => {
+        if (err) return res.status(500).json({ message: "Image must one" })
+        try {
+            if (!req.file) { 
+                const imageLink = req.body.imageLink
+                const data = req.body
+                const dataArticle = {
+                    title: data.title,
+                    content: data.content,
+                    category: data.category,
+                    tag: data.tag,
+                    image: imageLink
+                }
+                const editData = await Blog.updateOne({ _id: id }, {$set:dataArticle})
+                res.status(200).json({
+                    message: "successfully edit data",
+                    result: editData
+                })
+            } else {
+                const image = req.file
+                const imgBlog = `${path}/${image.filename}`
+                const data = req.body
+                const dataArticle = {
+                    title: data.title,
+                    content: data.content,
+                    category: data.category,
+                    tag: data.tag,
+                    image: imgBlog
+                }
+                const editData = await Blog.updateOne({ _id: id }, {$set:dataArticle})
+                res.status(200).json({
+                    message: "successfully edit data",
+                    result: editData
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(404).json({message: "failed to edit data"})
+        }
+    })
 }
 
 const deleteArticle = async (req, res) => {
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -3,9 +3,9 @@ const { createArticle,getAllArticle,getArticleById,editArticleById,deleteArticle
 const { authApiAdmin } = require('../controller/authorizeRoutes')
 
 
-route.post('/api/blog/create-article', authApiAdmin, createArticle);
+route.post('/api/blog/create-article', authApiAdmin,createArticle);
 route.get('/api/blog/articles', getAllArticle);
 route.get('/api/blog/articles/:id', getArticleById);
-route.patch('/api/blog/articles/update/:id', authApiAdmin, editArticleById);
+route.patch('/api/blog/articles/update/:id',authApiAdmin,  editArticleById);
 route.delete('/api/blog/articles/delete/:id', authApiAdmin, deleteArticle);
 module.exports = route
